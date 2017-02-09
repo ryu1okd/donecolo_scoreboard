d8bffce72ac01147570d6fb3b8b3e614d079b36c
ryu1okd <r122.project@gmail.com>
2017-02-09 14:52:37 +0900

    Run compile riot and pug.

---
+++ dist/tags/game_logs/game_logs.js
@@ -0,0 +1,4 @@
+
+riot.tag2('game_logs', '<div>GAME LOGS </div>', '', '', function(opts) {
+this.message = 'game_logs';
+});
---
+++ dist/tags/player/player.js
@@ -0,0 +1,4 @@
+
+riot.tag2('player', '<div class="player">{player} </div>', '', '', function(opts) {
+this.player = 'PLAYER';
+});
