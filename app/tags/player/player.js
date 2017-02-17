28a5903f478d88d401cc1c36132f6d0a5b7e6c87
ryu1okd <r122.project@gmail.com>
2017-02-18 08:17:51 +0900

    Add css Materialize.

--- app/tags/player/player.js
+++ app/tags/player/player.js
@@ -1,5 +1,5 @@
 
-riot.tag2('player', '<table> <thead> <th>name</th> <th>wins</th> <th>adv</th> <th>continuing</th> <th>set</th> </thead> <tbody> <tr> <td>{p1.name}</td> <td>{p1.wins}</td> <td> <span show="{p1.adv}">ADV</span></td> <td>{p1.continuing}</td> <td>{p1.set}</td> </tr> <tr> <td>{p2.name}</td> <td>{p2.wins}</td> <td><span show="{p2.adv}">ADV</span></td> <td>{p2.continuing}</td> <td>{p2.set}</td> </tr> </tbody> </table>', '', '', function(opts) {
+riot.tag2('player', '<div> <table> <thead> <th>name</th> <th>wins</th> <th><span class="truncate">continuous</span></th> <th>point</th> <th>set</th> </thead> <tbody> <tr> <td><span class="new badge red left" show="{p1.adv}" data-badge-caption="ADV"> </span> <input id="p1_name" riot-value="{p1.name}" placeholder="Player1" type="text"> </td> <td>{p1.wins}</td> <td>{p1.continuous}</td> <td>{p1.point}</td> <td>{p1.set}</td> </tr> <tr> <td> <span class="new badge blue left" show="{p2.adv}" data-badge-caption="ADV"> </span> <input id="p2_name" riot-value="{p2.name}" placeholder="Player2" type="text"> </td> <td>{p2.wins}</td> <td>{p2.continuous}</td> <td>{p2.point}</td> <td>{p2.set}</td> </tr> </tbody> </table> </div>', '', '', function(opts) {
 var ref$, map, filter, last, this$ = this;
 ref$ = require('prelude-ls'), map = ref$.map, filter = ref$.filter, last = ref$.last;
 this.on('before-mount', function(){
@@ -9,7 +9,8 @@ this.on('before-mount', function(){
     adv: false,
     game_logs: [],
     wins: 0,
-    continuing: 0,
+    continuous: 0,
+    point: 0,
     set: 0
   };
   return this.p2 = {
@@ -17,7 +18,8 @@ this.on('before-mount', function(){
     adv: false,
     game_logs: [],
     wins: 0,
-    continuing: 0,
+    continuous: 0,
+    point: 0,
     set: 0
   };
 });
@@ -35,16 +37,22 @@ this.update_info = function(p){
   })(
   last(
   p.game_logs));
-  p.continuing = p.adv && (function(it){
+  p.continuous = p.adv && (function(it){
     return it === 'win';
   })(
   last(
-  p.game_logs)) ? p.continuing + 1 : 0;
-  if (p.continuing === 3) {
-    p.set += 1;
-    p.continuing = 0;
+  p.game_logs)) ? p.continuous + 1 : 0;
+  if (p.continuous === 3) {
+    p.point += 1;
   }
-  console.log(JSON.stringify(p));
+  if (p.continuous === 2) {
+    p.point += 1;
+  }
+  if (p.point === 2) {
+    p.set += 1;
+    p.continuous = 0;
+    p.point = 0;
+  }
 };
 this.on('update', function(){
   this.update_info(this.p1);
--- src/tags/player/player.ls
+++ src/tags/player/player.ls
@@ -7,25 +7,32 @@ require! \prelude-ls : {map, filter, last}
     adv: false
     game_logs: []
     wins: 0
-    continuing: 0
+    continuous: 0
+    point: 0
     set: 0
   @p2 =
     name: \Player2
     adv: false
     game_logs: []
     wins: 0
-    continuing: 0
+    continuous: 0
+    point: 0
     set: 0
 
 @update_info = (p) !->
   p.wins = p.game_logs |> filter (is \win) |> ( .length)
   p.adv = p.game_logs |> last |> ( is \win)
 
-  p.continuing = if p.adv and (p.game_logs |> last |> ( is \win)) then p.continuing + 1 else 0
-  if p.continuing is 3
+  p.continuous = if p.adv and (p.game_logs |> last |> ( is \win)) then p.continuous + 1 else 0
+  if p.continuous is 3
+    p.point += 1
+  if p.continuous is 2
+    p.point += 1
+  if p.point is 2
     p.set += 1
-    p.continuing = 0
-  console.log JSON.stringify p
+    p.continuous = 0
+    p.point = 0
+  #console.log JSON.stringify p
 
 @on \update, !->
   @update_info @p1
